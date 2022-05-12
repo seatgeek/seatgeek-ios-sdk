99712c14d9e9caa8b3983002681172b9aa650a48
Daniel Barden <dbarden@seatgeek.com>
2022-05-12 14:21:52 +0200

    Creates a dynamic package

--- Package.swift
+++ Package.swift
@@ -11,23 +11,27 @@ let package = Package(
     products: [
         .library(
             name: "SeatGeek",
+            type: .dynamic,
             targets: ["SeatGeek"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/danielgindi/Charts.git", .upToNextMajor(from: "3.4.0")),
-        .package(url: "https://github.com/bmoliveira/MarkdownKit.git", from: "1.7.0"),
-        .package(url: "https://github.com/lachlanbell/SwiftOTP.git", .upToNextMinor(from: "3.0.0"))
+        .package(url: "https://github.com/dbarden/Charts.git", .upToNextMajor(from: "3.6.1")),
+        .package(url: "https://github.com/dbarden/MarkdownKit.git", branch: "master"),
+        .package(url: "https://github.com/dbarden/SwiftOTP.git", branch: "master")
     ],
     targets: [
         .binaryTarget(
             name: "SeatGeekSDK",
             url: "https://seatgeek.jfrog.io/artifactory/ios-sdk/v4.0.0/SeatGeekSDK.xcframework.zip",
-            checksum: "765813de02a4e3b9abcfd27d9177f3eb3d3c992135d99ecf1b4971d339a8400a"
+            checksum: "fd5206cd9ea1dd6d0cd6bf13dc63985374e0a8731d20f58cf7dffd2710419257"
         ),
         .target(name: "SeatGeek",
                 dependencies: [
-                    "SnapKit", "Charts", "MarkdownKit", "SwiftOTP",
+                    .product(name: "SnapKit-Dynamic", package: "SnapKit"),
+                    .product(name: "Charts-Dynamic", package: "Charts"),
+                    .product(name: "MarkdownKit-Dynamic", package: "MarkdownKit"),
+                    .product(name: "SwiftOTP-Dynamic",package: "SwiftOTP"),
                     .target(name: "SeatGeekSDK")
                 ],
                 path: "Sources/SeatGeekSDK/")
