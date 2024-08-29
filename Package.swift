30edbfa25d2cd3e41ce48dffca413a37720d389d
Aliaksandr Tserashkou <162339701+atserashkou-sg@users.noreply.github.com>
2024-08-29 17:10:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,48 +11,28 @@ let package = Package(
     products: [
         .library(
             name: "SeatGeek",
-            targets: ["_SeatGeekSDKTarget"])
+            targets: ["_SeatGeekSDKTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-http-types", exact: "1.2.0"),
+        .package(url: "https://github.com/lachlanbell/SwiftOTP", exact: "3.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit", exact: "5.7.1")
     ],
     targets: [
         .target(
             name: "_SeatGeekSDKTarget",
-            dependencies: [.target(name: "_SeatGeekSDKWrapper",
-                                   condition: .when(platforms: [.iOS]))]
-        ),
-        .target(
-            name: "_SeatGeekSDKWrapper",
             dependencies: [
-                "SeatGeekSDKTG",
-                "SnapKitTarget",
-                "SwiftOTPTarget",
-                "HTTPTypesFoundationTarget",
-                "HTTPTypesTarget"
-            ]
-        ),
+                .target(name: "SeatGeekSDK"),
+                .product(name: "HTTPTypes", package: "swift-http-types"),
+                .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "SwiftOTP", package: "SwiftOTP")
+            ]),
+  
         .binaryTarget(
-            name: "SeatGeekSDKTG",
-            url: "https://seatgeek.jfrog.io/artifactory/sdk-ios/4.6.0/SeatGeekSDK.xcframework.zip",
-            checksum: "71146013fce754ccbfbdccaa64acee3ecb73c88e967c205b949d05e6c574c803"
-        ),
-        .binaryTarget(
-            name: "SnapKitTarget",
-            url: "https://seatgeek.jfrog.io/artifactory/snapkit-ios/v5.7.1/SnapKit-signed.xcframework.zip",
-            checksum: "0a9acdae73aa612b685f704a49a0aaae11c8770e08f0c9e9954342bf9ed30fd8"
-        ),
-        .binaryTarget(
-            name: "SwiftOTPTarget",
-            url: "https://seatgeek.jfrog.io/artifactory/swiftotp-ios/v3.0.0/SwiftOTP.xcframework.zip",
-            checksum: "94bd56c16c25346e4eb490090ed2a63717b4d60eb6bed7b1493acdd0b11081e7"
-        ),
-        .binaryTarget(
-            name: "HTTPTypesFoundationTarget",
-            url: "https://seatgeek.jfrog.io/artifactory/swift-http-types/swift-http-types-foundation.xcframework.zip",
-            checksum: "f8ad38cdf7611b4868873f60a0cd08b3ce6980301755b98fd345a1a6e650123b"
-        ),
-        .binaryTarget(
-            name: "HTTPTypesTarget",
-            url: "https://seatgeek.jfrog.io/artifactory/swift-http-types/swift-http-types.xcframework.zip",
-            checksum: "2c7677588c1e56bc83348a298333663d2430c33abb32fedaf36050848970d3dd"
+            name: "SeatGeekSDK",
+            url: "https://seatgeek.jfrog.io/artifactory/sdk-ios/test-29-08/SeatGeekSDK.xcframework.zip",
+            checksum: "c7d63333d216c69a08a527d2af544ea4c44add217cc0f001b4c6d2dcffc1e630"
         )
     ]
 )
