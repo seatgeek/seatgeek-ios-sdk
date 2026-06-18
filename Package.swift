f1b40a665ee6715cb1578963196e82bf4d4af6d4
Aliaksandr Tserashkou <162339701+atserashkou-sg@users.noreply.github.com>
2026-06-18 16:52:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -31,8 +31,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SeatGeekSDKTG",
-            url: "https://seatgeek.jfrog.io/artifactory/sdk-ios/5.4.0/SeatGeekSDK.xcframework.zip",
-            checksum: "db5a95350942b1f24049346a8324578b5a0ed6ed87896e41960a6ee96ffc53d1"
+            url: "https://seatgeek.jfrog.io/artifactory/sdk-ios/5.4.1/SeatGeekSDK.xcframework.zip",
+            checksum: "4f6974844aff0bfeeb7f2fc90ddd63eae180a854d09e4fc32197e0c3758cf2a8"
         ),
         .binaryTarget(
             name: "SnapKitTarget",
